3c50b7b962c67840345fdd869b43505e3354b140
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2024-11-05 21:41:37 +0200

    Add comment to function as requested

--- uppercase.go
+++ uppercase.go
@@ -2,6 +2,8 @@ package main
 
 import "strings"
 
+// Upper converts strings to their upper case equivalent
+// using built in strings.ToUpper, so don't ask me how it works
 func Upper(val string) string {
 	return strings.ToUpper(val)
 }
